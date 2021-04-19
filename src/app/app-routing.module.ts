01d574e64279f16b8fc8485606e634635552df43
_Comm <_Comm@ba.loc>
2021-04-19 14:53:15 +0200

    init template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SinginComponent } from './components/auth/singin/singin.component';
+import { SingupComponent } from './components/auth/singup/singup.component';
+import { NotFoundComponent } from './components/partiels/not-found/not-found.component';
+import { AddProductComponent } from './components/shop/add-product/add-product.component';
+import { CartComponent } from './components/shop/cart/cart.component';
+import { EditProductComponent } from './components/shop/edit-product/edit-product.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { SingleProductComponent } from './components/shop/single-product/single-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'singup', component: SingupComponent},
+  {path:'singin', component: SinginComponent},
+  {path:'shop', component: ShopComponent},
+  {path:'add-product', component: AddProductComponent},
+  {path:'single-product/:id', component: SingleProductComponent},
+  {path:'edit-product/:id', component: EditProductComponent},
+  {path:'cart', component: CartComponent},
+  {path:'**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
