511b75635baca62e9b1b55363fd75c19b1e6cf3a
_Comm <_Comm@ba.loc>
2021-04-19 12:21:07 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SinginComponent } from './components/auth/singin/singin.component';
+import { SingupComponent } from './components/auth/singup/singup.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { SingleProductComponent } from './components/shop/single-product/single-product.component';
+import { EditProductComponent } from './components/shop/edit-product/edit-product.component';
+import { CartComponent } from './components/shop/cart/cart.component';
+import { AddProductComponent } from './components/shop/add-product/add-product.component';
+import { HeaderComponent } from './components/partiels/header/header.component';
+import { FooterComponent } from './components/partiels/footer/footer.component';
+import { NotFoundComponent } from './components/partiels/not-found/not-found.component';
+import { HeaderPageComponent } from './components/partiels/header-page/header-page.component';
+import { QuickViewModalComponent } from './components/partiels/modal/quick-view-modal/quick-view-modal.component';
+import { AddToCartModalComponent } from './components/partiels/modal/add-to-cart-modal/add-to-cart-modal.component';
+import { DeleteProductModalComponent } from './components/partiels/modal/delete-product-modal/delete-product-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SinginComponent,
+    SingupComponent,
+    ShopComponent,
+    SingleProductComponent,
+    EditProductComponent,
+    CartComponent,
+    AddProductComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HeaderPageComponent,
+    QuickViewModalComponent,
+    AddToCartModalComponent,
+    DeleteProductModalComponent
   ],
   imports: [
     BrowserModule,
